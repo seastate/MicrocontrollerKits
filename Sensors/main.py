53dc2bc43bc145f7149552447d25e24d2190b5fd
Robert Levine <leviner@uw.edu>
2020-12-14 14:48:53 -0800

    merge multi_main and main

--- Sensors/main.py
+++ Sensors/main.py
@@ -1,22 +1,36 @@
 # Initialize microcontrollers for PublicSensors/SensoresPublicos temperature activities
 
 from platform_defs import *
+import active_sensors
 
+from sys import print_exception
 from machine import I2C
 from time import sleep
 from esp8266_i2c_lcd import I2cLcd
-import active_sensors
 
 def main():
-    #activeName, activeFuncs =  [[] for i in range(2)] For when we are going to use multiple sensors
+    global sensor # use a global variable to fix global vs. local namespace issues
+    num_sensors=0  # number of sensors successfuly initialized
+    activeNames, activeFuncs, sensors =  [[] for i in range(3)]  # Lists for when we are going to use multiple sensors
+    #activeNames = []
+    #activeFuncs = []
+    #sensors = []
     sensorFuncs = {'light': 'light', 'distance': 'dist', 'temperature': 'temp', 'GPS': 'GPS'}
     for sensr in [item for item in dir(active_sensors) if not item.startswith("__")]:
         if eval('active_sensors.'+sensr) == 1:
-            activeName = sensr
-            activeFuncs = sensorFuncs[sensr]
-    exec('import read_'+activeFuncs)
-    exec('sensor = read_'+activeFuncs+'.read_'+activeFuncs+'()')
-    sleep(1)
+            try:
+                activeName = sensr
+                activeFunc = sensorFuncs[sensr]
+                exec('import read_'+activeFunc)
+                sensor = eval('read_'+activeFunc+'.read_'+activeFunc+'()')
+                sleep(1)
+                print('success: queuing sensor driver ',activeFunc)
+                num_sensors+=1
+                activeNames.append(activeName)
+                activeFuncs.append(activeFunc)
+                sensors.append(sensor)
+            except:
+                print('Error: sensor driver ',activeFunc,' was requested but failed to load')
     try:
         i2c = I2C(scl=Pin(p_I2Cscl_lbl),sda=Pin(p_I2Csda_lbl))
         lcd = I2cLcd(i2c, 0x27,2,16)
@@ -25,15 +39,20 @@ def main():
         lcd.clear()
         lcd.putstr("Ready!\n"+chr(0)+'Listo!')
 
+        i=0
         while True:
             first = button.value()
             sleep(0.01)
             second = button.value()
             if first and not second:
-                exec('sensor.print_'+activeFuncs+'()')
+                sensor=sensors[i]
+                exec('sensor.print_'+activeFuncs[i]+'()')
+                i = (i+1) % num_sensors
             elif not first and second:
                 pass
-    except:
+    except Exception as e:
+        print_exception(e)
         print('LCD not registered...')
+
 
 main()
